57e1c3809c04e6a1608ab75af3b84e36552cfdb1
Matt “Kalyp3r” Taylor <mrsymtec@gmail.com>
2024-02-14 17:44:02 +1300

    Correcting erorrs in code

--- Snake/Snake.cpp
+++ Snake/Snake.cpp
@@ -1,128 +1,98 @@
-// Snake.cpp :
-
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include <conio.h>
-#include <stdio.h>
-#include <Windows.h>
-//This is the basic structure of the game such as Variables and games intial state
-bool gameOver;
+#include <windows.h>
 
+using namespace std;
+
+bool gameOver;
 const int width = 20;
 const int height = 20;
-
 int x, y, fruitX, fruitY, score;
-
 int tailX[100], tailY[100];
 int nTail;
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+eDirection dir;
 
-std::string fruit = "*";
-
-enum eDirections
-{
-    STOP = 0,
-    LEFT,
-    RIGHT,
-    UP,
-    DOWN
-};
-
-eDirections dir;
-
-void Setup()
-{
+void Setup() {
     gameOver = false;
     dir = STOP;
     x = width / 2;
     y = height / 2;
-    fruitX = rand() % width + 1;
-    fruitY = rand() % height + 1;
+    srand(time(NULL));
+    fruitX = rand() % width;
+    fruitY = rand() % height;
     score = 0;
+    nTail = 0;
 }
 
-//This renders the game enviroment, displays the snake and the fruit and score
-void Draw()
-{
+void Draw() {
     system("cls");
     for (int i = 0; i < width + 2; i++)
-    {
-        std::cout << "#";
-    }
-    std::cout << std::endl;
-    for (int i = 1; i <= height; i++)
-    {
-        for (int j = 1; j <= width + 1; j++)
-        {
-            if (j == 1 || j == width + 1)
-                std::cout << "#";
+        cout << "#";
+    cout << endl;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (j == 0)
+                cout << "#";
             if (i == y && j == x)
-            {
-                std::cout << "O";
-            }
-            else if (j == fruitX && i == fruitY)
-            {
-                std::cout << fruit;
-            }
-            else
-            {
+                cout << "O";
+            else if (i == fruitY && j == fruitX)
+                cout << "*";
+            else {
                 bool print = false;
-                for (int k = 0; k < nTail; k++)
-                {
-                    if (tailX[k] == j && tailY[k] == i)
-                    {
-                        std::cout << "o";
+                for (int k = 0; k < nTail; k++) {
+                    if (tailX[k] == j && tailY[k] == i) {
+                        cout << "o";
                         print = true;
                     }
                 }
                 if (!print)
-                    std::cout << " ";
+                    cout << " ";
             }
+            if (j == width - 1)
+                cout << "#";
         }
-        std::cout << std::endl;
+        cout << endl;
     }
+
     for (int i = 0; i < width + 2; i++)
-    {
-        std::cout << "#";
-    }
-    std::cout << std::endl;
-    std::cout << "Score: " << score << std::endl;
+        cout << "#";
+    cout << endl;
+    cout << "Score:" << score << endl;
 }
-//This handles the users input 
-void Input()
-{
-    if (_kbhit())
-    {
-        switch (_getch())
-        {
-        case 'a':
-            dir = LEFT;
-            break;
-        case 'd':
-            dir = RIGHT;
-            break;
-        case 'w':
-            dir = UP;
-            break;
-        case 's':
-            dir = DOWN;
-            break;
-        case 'x':
-            gameOver = true;
-            break;
+
+void Input() {
+    if (_kbhit()) {
+        switch (_getch()) {
+            case 'a':
+                dir = LEFT;
+                break;
+            case 'd':
+                dir = RIGHT;
+                break;
+            case 'w':
+                dir = UP;
+                break;
+            case 's':
+                dir = DOWN;
+                break;
+            case 'x':
+                gameOver = true;
+                break;
         }
     }
 }
 
-//This handles the updates to the games logic including moving the snake 
-//detecting collisions and updating the score
-void Logic()
-{
+void Logic() {
     int prevX = tailX[0];
     int prevY = tailY[0];
     int prev2X, prev2Y;
     tailX[0] = x;
     tailY[0] = y;
-    for (int i = 1; i < nTail; i++)
-    {
+    for (int i = 1; i < nTail; i++) {
         prev2X = tailX[i];
         prev2Y = tailY[i];
         tailX[i] = prevX;
@@ -130,57 +100,45 @@ void Logic()
         prevX = prev2X;
         prevY = prev2Y;
     }
-    switch (dir)
-    {
-    case LEFT:
-        x--;
-        break;
-    case RIGHT:
-        x++;
-        break;
-    case UP:
-        y--;
-        break;
-    case DOWN:
-        y++;
-        break;
+    switch (dir) {
+        case LEFT:
+            x--;
+            break;
+        case RIGHT:
+            x++;
+            break;
+        case UP:
+            y--;
+            break;
+        case DOWN:
+            y++;
+            break;
+        default:
+            break;
     }
-    if ((x > width || x < 0) || (y > height || y < 0))
-    {
-        std::cout << "\t\tGame Over" << std::endl;
-        std::cout << "\t    You Hit the wall!";
-        gameOver = true;
-    }
+
+    if (x >= width) x = 0; else if (x < 0) x = width - 1;
+    if (y >= height) y = 0; else if (y < 0) y = height - 1;
+
     for (int i = 0; i < nTail; i++)
-    {
         if (tailX[i] == x && tailY[i] == y)
-        {
-            std::cout << "\t\tGame Over" << std::endl;
-            std::cout << "\t    You Hit the Tail!";
             gameOver = true;
-        }
-    }
-    if (x == fruitX && y == fruitY)
-    {
+
+    if (x == fruitX && y == fruitY) {
         score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
         nTail++;
-        fruitX = rand() % width + 1;
-        fruitY = rand() % height + 1;
     }
 }
 
-//Here is where the game is run. 
-
-int main()
-{
+int main() {
     Setup();
-    while (!gameOver)
-    {
+    while (!gameOver) {
         Draw();
         Input();
         Logic();
-        Sleep(150);
+        Sleep(150); // Milliseconds
     }
-    std::cout << fruitX << " " << fruitY;
     return 0;
 }
